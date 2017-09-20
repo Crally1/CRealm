9ec71b20bc7be0e1a2153d134c8f9257b135f51f
crally <jameswhitfield65@gmail.com>
2017-09-19 18:20:23 -0600

    Another test.

--- src/world.cpp
+++ src/world.cpp
@@ -2,8 +2,8 @@
 
 World::World(sf::RenderWindow &window) :mWindow(window), mWorldView(window.getDefaultView()),
     mWorldBounds(0.f, 0.f, mWorldView.getSize().x, 2000.f),
-    mSpawnPosition(mWorldView.getSize().x / 2.f, mWorldBounds.height - mWorldView.getSize().y),
-            mPlayerEntity(nullptr)
+    mSpawnPosition(mWorldView.getSize().x / 2.f, mWorldBounds.height - mWorldView.getSize().y), 
+    mPlayerEntity(nullptr)
 {
     loadTextures();
     buildScene();
